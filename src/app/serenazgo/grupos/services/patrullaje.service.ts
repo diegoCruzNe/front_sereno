d5aba45fa6fffc177669565ae2d5acce6268684a
diego <diego_fcb_96@hotmail.com>
2023-08-11 02:14:06 -0500

    table add patrullaje into sereno

--- src/app/serenazgo/grupos/services/patrullaje.service.ts
+++ src/app/serenazgo/grupos/services/patrullaje.service.ts
@@ -6,6 +6,16 @@ import { Observable } from 'rxjs';
 import { getPatrullaje } from 'src/app/interfaces/getPatrullaje.interface';
 import { newPatrullaje } from 'src/app/interfaces/newPatrullaje.interface';
 
+ interface PatrullajeDetails {
+  id_patrullaje: number;
+  descripcion: null | string;
+  turno: string;
+  tipo_unidad: string;
+  placa: string;
+  fecha_creacion: string;
+  num_sere: number;
+ }
+
 interface resNewPatru {
   ok:  boolean;
   msg: string;
@@ -32,5 +42,9 @@ export class PatrullajeService {
 
   newPatrullaje(patrullaje: newPatrullaje): Observable<resNewPatru> {
     return this.http.post<resNewPatru>(`${this.url}/patrullaje`, patrullaje, this.headers);
+  }
+
+  getPatrullajeDetalles(): Observable<PatrullajeDetails[]> {
+    return this.http.get<PatrullajeDetails[]>(`${this.url}/patrullaje_det`, this.headers);
   }
 }
--- src/app/serenazgo/serenos/select-group/select-group.component.ts
+++ src/app/serenazgo/serenos/select-group/select-group.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PatrullajeService } from '../../grupos/services/patrullaje.service';
+import { MatTableDataSource } from '@angular/material/table';
+
+export interface PatrullajeDetails {
+  id_patrullaje: number;
+  descripcion: null | string;
+  turno: string;
+  tipo_unidad: string;
+  placa: string;
+  fecha_creacion: string;
+  num_sere: number;
+}
 
 @Component({
   selector: 'app-select-group',
   templateUrl: './select-group.component.html',
-  styleUrls: ['./select-group.component.css']
+  styleUrls: ['./select-group.component.css'],
 })
-export class SelectGroupComponent {
+export class SelectGroupComponent implements OnInit {
+  colum_patru: string[] = ['turno', 'unidad', 'tipo_uni', 'num_sere'];
+  clickedFilas: PatrullajeDetails[] = [];
+  patrullajes: MatTableDataSource<PatrullajeDetails>;
 
+  constructor(private patrullajeService: PatrullajeService) {
+    this.patrullajes = new MatTableDataSource();
+  }
+
+  ngOnInit() {
+    this.getPatrullajesDet();
+  }
+
+  getPatrullajesDet() {
+    this.patrullajeService
+      .getPatrullajeDetalles()
+      .subscribe((res_patru: PatrullajeDetails[]) => {
+        this.patrullajes.data = res_patru;
+        console.log(res_patru);
+      });
+  }
 }
